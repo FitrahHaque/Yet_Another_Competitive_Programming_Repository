12557c244b97b548b7727fa431e1aa4434254c63
Asif Ihtemadul Haque <asifihtemadulhaque@gmail.com>
2023-07-06 12:36:32 +0600

    BUET individual 6.7.23

---
+++ BUET/Individual 6.7.23/J.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+#include<bits/extc++.h>
+
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define nline "\n"
+#define pb push_back
+#define mp make_pair
+#define all(a) (a).begin(),(a).end()
+#define ff first
+#define ss second
+#define repv(i, m) for(auto (i) = (m).begin();(i) != (m).end();(i)++)
+#define FileIO freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+string to_string(char x) { return (string) "\'" + x + "\'"; }
+
+string to_string(const string &x) { return '\"' + x + '\"'; }
+
+string to_string(const char *x) { return to_string((string) x); }
+
+string to_string(bool x) { return x ? "true" : "false"; }
+
+template<typename T, typename V>
+string to_string(const pair <T, V> &x) { return '(' + to_string(x.first) + ',' + to_string(x.second) + ')'; }
+
+template<typename T>
+string to_string(const T &x) {
+    int f = 0;
+    string r;
+    for (auto &i: x) r += (f++ ? "," : "") + to_string(i);
+    return '{' + r + '}';
+}
+
+void _print() { cerr << "]\n"; }
+
+template<typename T, typename... V>
+void _print(T t, V... v) {
+    cerr << to_string(t);
+    if (sizeof...(v)) cerr << ", ";
+    _print(v...);
+}
+
+template<typename T>
+using ordered_set = tree <T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...)  cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(...)
+#endif
+const long long INFL = 4000000000000000000LL;
+const int INF = 2047483647;
+const long long mod = 1000000007LL;
+
+/********************************************* this is boring *********************************************/
+
+void solve(int tc) {
+	int n,m,k;
+	cin >> n >> m >> k;
+	vector<int> a(n);
+	for(auto &i:a)
+		cin >> i;
+	int points = k;
+	for(int i=0;i<m;i++){
+		if(a[i] == 0){
+			points = 0;
+			break;
+		}
+	}
+	if(points == 0){
+		cout << points << endl;
+		return;
+	}
+	points = 100;
+	for(int i=m;i<n;i++){
+		if(a[i] == 0){
+			points = k;
+			break;
+		}
+	}
+	cout << points << endl;
+}
+
+int main() {
+//#ifndef ONLINE_JUDGE
+//    FileIO
+//#endif
+    IOS
+    cout << setprecision(15) << fixed;
+
+    int tc = 1;
+   cin >> tc;
+    for (int t = 1; t <= tc; t++) solve(t);
+//    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s\n";
+}
